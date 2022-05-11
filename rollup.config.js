192e16ed31fc1e0cc6efab6b0f81002a3e95e504
BrettThurs10 <brettmarcthurston@gmail.com>
2022-05-11 14:54:35 -0500

    remove npmrc file

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,23 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import postcss from "rollup-plugin-postcss";
 import del from "rollup-plugin-delete";
-const packageJson = require("./package.json");
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    del({ targets: "dist/*" }),
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({
-      tsconfig: "tsconfig.build.json",
-      useTsconfigDeclarationDir: true,
-    }),
-    postcss(),
-  ],
-};
+import pkg from "./package.json";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "playground/src/component-lib/index.js",
+        format: "esm",
+        banner: "/* eslint-disable */",
+      },
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "esm" },
+    ],
+    plugins: [
+      del({ targets: ["dist/*", "playground/src/component-lib"] }),
+      typescript(),
+    ],
+    external: Object.keys(pkg.peerDependencies || {}),
+  },
+];
